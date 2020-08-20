862225e7be76aa1d8f34f6c40eebe5284e29a229
Farhan Farooq <farhan.farooq@live.com>
2020-08-21 00:42:19 +0500

    added search functionality in home page and category pages

--- src/components/products.js
+++ src/components/products.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React, { useState, Fragment } from "react"
 import { Link } from "gatsby"
 import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
 import { makeStyles } from "@material-ui/core/styles"
 import Img from "gatsby-image"
 
 const useStyles = makeStyles(() => ({
+  searchContainer: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "20px 0px",
+  },
+  search: {
+    width: "50%",
+  },
   main: {
     display: "flex",
     justifyContent: "space-between",
@@ -32,23 +41,80 @@ const useStyles = makeStyles(() => ({
 const Products = ({ data }) => {
   const classes = useStyles()
 
+  const [search, setSearch] = useState("")
+  const [products, setProducts] = useState([])
+
+  const handleSearch = e => {
+    setSearch(e.target.value)
+
+    const productsArray = data.allContentfulProducts.nodes.filter(product =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    )
+
+    setProducts(productsArray)
+  }
+
+  const renderData = (catTitle, slug, img, title, price) => (
+    <Link to={`/category/${catTitle}/${slug}`} className={classes.link}>
+      <div>
+        <Img fixed={img} className={classes.image} />
+        <Typography className={classes.title}>{title}</Typography>
+        <Typography>{price}</Typography>
+      </div>
+    </Link>
+  )
+
   return (
-    <div className={classes.main}>
-      {data.allContentfulProducts.nodes.map(node => (
-        <div key={node.title}>
-          <Link
-            to={`/category/${node.category.title.toLowerCase()}/${node.slug}`}
-            className={classes.link}
-          >
-            <div>
-              <Img fixed={node.image.fixed} className={classes.image} />
-              <Typography className={classes.title}>{node.title}</Typography>
-              <Typography>$ {node.price}</Typography>
-            </div>
-          </Link>
-        </div>
-      ))}
-    </div>
+    <>
+      {/* Search Field */}
+      <div className={classes.searchContainer}>
+        <TextField
+          value={search}
+          onChange={handleSearch}
+          label="Search"
+          variant="outlined"
+          className={classes.search}
+          size="small"
+          placeholder="Search Product eg: maroon plimsoll"
+        />
+      </div>
+      {/* End search field */}
+
+      <div className={classes.main}>
+        {/* Search results */}
+        {products.length > 0 &&
+          search !== "" &&
+          products.map(product => (
+            <Fragment key={product.title}>
+              {renderData(
+                product.category.title.toLowerCase(),
+                product.slug,
+                product.image.fixed,
+                product.title,
+                product.price
+              )}
+            </Fragment>
+          ))}
+        {/* End search results */}
+
+        {/* All products */}
+        {search === "" &&
+          data.allContentfulProducts.nodes.map(node => {
+            return (
+              <Fragment key={node.title}>
+                {renderData(
+                  node.category.title.toLowerCase(),
+                  node.slug,
+                  node.image.fixed,
+                  node.title,
+                  node.price
+                )}
+              </Fragment>
+            )
+          })}
+        {/* End all products */}
+      </div>
+    </>
   )
 }
 export default Products
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link, graphql } from "gatsby"
 import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
 import { makeStyles } from "@material-ui/core/styles"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
@@ -9,6 +10,14 @@ import Loader from "../components/loader"
 import "./index.css"
 
 const useStyles = makeStyles(() => ({
+  searchContainer: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "20px 0px",
+  },
+  search: {
+    width: "50%",
+  },
   main: {
     display: "flex",
     justifyContent: "space-between",
@@ -33,12 +42,23 @@ const useStyles = makeStyles(() => ({
     borderTopLeftRadius: "10px",
     borderTopRightRadius: "10px",
   },
+  searchTitle: {
+    fontSize: "20px",
+    fontWeight: "bold",
+    opacity: "0.7",
+  },
+  link: {
+    textDecoration: "none",
+    color: "black",
+  },
 }))
 
 const IndexPage = ({ data }) => {
   const classes = useStyles()
 
   const [render, setRender] = useState(false)
+  const [search, setSearch] = useState("")
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     setTimeout(() => {
@@ -46,18 +66,68 @@ const IndexPage = ({ data }) => {
     }, 1000)
   }, [])
 
+  const handleSearch = e => {
+    setSearch(e.target.value)
+
+    const productsArray = data.allContentfulProducts.nodes.filter(product =>
+      product.title.toLowerCase().includes(search.toLowerCase())
+    )
+
+    setProducts(productsArray)
+  }
+
   return render ? (
     <Layout>
       <SEO title="Home" />
+      {/* Search Field */}
+      <div className={classes.searchContainer}>
+        <TextField
+          value={search}
+          onChange={handleSearch}
+          label="Search"
+          variant="outlined"
+          className={classes.search}
+          size="small"
+          placeholder="Search Product eg: maroon plimsoll"
+        />
+      </div>
+      {/* End search field */}
+
       <div className={classes.main}>
-        {data.allContentfulCategories.nodes.map(node => (
-          <div style={{ position: "relative" }} key={node.title}>
-            <Link to={`/category/${node.title.toLowerCase()}`}>
-              <Img fixed={node.image.fixed} className={classes.image} />
-              <Typography className={classes.title}>{node.title}</Typography>
-            </Link>
-          </div>
-        ))}
+        {/* Search results */}
+        {products.length > 0 &&
+          search !== "" &&
+          products.map(product => (
+            <div key={product.title}>
+              <Link
+                to={`/category/${product.category.title.toLowerCase()}/${
+                  product.slug
+                }`}
+                className={classes.link}
+              >
+                <div>
+                  <Img fixed={product.image.fixed} className={classes.image} />
+                  <Typography className={classes.searchTitle}>
+                    {product.title}
+                  </Typography>
+                  <Typography>$ {product.price}</Typography>
+                </div>
+              </Link>
+            </div>
+          ))}
+        {/* End search results */}
+
+        {/* Categories */}
+        {search === "" &&
+          data.allContentfulCategories.nodes.map(node => (
+            <div style={{ position: "relative" }} key={node.title}>
+              <Link to={`/category/${node.title.toLowerCase()}`}>
+                <Img fixed={node.image.fixed} className={classes.image} />
+                <Typography className={classes.title}>{node.title}</Typography>
+              </Link>
+            </div>
+          ))}
+        {/* End categories */}
       </div>
     </Layout>
   ) : (
@@ -74,6 +144,24 @@ export const data = graphql`
           fixed(width: 300, height: 300) {
             ...GatsbyContentfulFixed_withWebp
           }
+        }
+      }
+    }
+    allContentfulProducts {
+      nodes {
+        title
+        slug
+        description {
+          description
+        }
+        price
+        image {
+          fixed(width: 300, height: 300) {
+            ...GatsbyContentfulFixed_withWebp
+          }
+        }
+        category {
+          title
         }
       }
     }
